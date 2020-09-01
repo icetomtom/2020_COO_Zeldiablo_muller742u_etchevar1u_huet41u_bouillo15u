5611f5a1097cec51ef079de8879f92967104a002
Léo Muller <leomllr8@gmail.com>
2020-09-01 17:02:25 +0200

    Tests pour la case piege

---
+++ projet_zeldiablo/src/Test/TestCase.java
@@ -0,0 +1,47 @@
+package Test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import Elements.Case;
+import Elements.CasePiege;
+import Partie.Jeu;
+
+/** 
+ * test la classe Case
+ */
+public class TestCase {
+	/**
+	 * test le constructeur avec les valeurs par defauts
+	 */
+	@Test
+	public void test_ConstructeurDefaut() {
+		Case c = new Case(0, 1, Case.TYPE_VIDE);
+		
+		assertEquals("la valeur par defaut n est pas bonne", 0, c.getX());
+		assertEquals("la valeur par defaut n est pas bonne", 1, c.getY());
+		assertEquals("la valeur n'est pas bonne", Case.TYPE_VIDE, c.getType());
+		
+	}
+
+	/**
+	 * test des cases pieges
+	 */
+	@Test
+	public void test_CasePiege() {
+		//initialisation
+		CasePiege c = new CasePiege(1, 1, 3);
+		Jeu j = new Jeu();
+		j.getLaby().setCase(c);
+		
+		//verification
+		for(int i = 0; i < 4; i++) {
+			j.deplacerJoueur(Jeu.LEFT);
+		}
+		
+		for(int i = 0; i < 4; i++) {
+			j.deplacerJoueur(Jeu.UP);
+		}
+	}
+}
