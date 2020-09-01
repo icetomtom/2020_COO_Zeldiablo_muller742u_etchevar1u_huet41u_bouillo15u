796d1dce123459e6fadedd19b0b7d81529bfc176
Léo Muller <leomllr8@gmail.com>
2020-09-01 17:14:19 +0200

    Modif de la methode activerEffet

--- projet_zeldiablo/src/Elements/CasePiege.java
+++ projet_zeldiablo/src/Elements/CasePiege.java
@@ -1,14 +1,21 @@
 package Elements;
 
 public class CasePiege extends CaseAEffet {
+	private boolean active;
 
 	public CasePiege(int x, int y, int type) {
 		super(x, y, type);
+		this.active = false;
 	}
 
 	@Override
 	public void activerEffet(Entite e) {
-		System.out.println("L'entite a ete blesse");
+		e.vie -= 1;
+		
+		if(!this.active) {
+			this.active = true;
+		}
 	}
-
+	
+	public boolean estActive() { return this.active; }
 }
