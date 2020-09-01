88bbaef12e0dc8ceff5550cbd4062642847eff8b
= <leomllr8@gmail.com>
2020-09-01 14:30:26 +0200

    Ajout de la classe Jeu et de la classe TestJeu

--- projet_zeldiablo/src/Elements/Jeu.java
+++ projet_zeldiablo/src/Elements/Jeu.java
@@ -1,6 +1,85 @@
 package Elements;
 
-
+/**
+ * 
+ * classe permettant de gerer le jeu
+ *
+ */
 public class Jeu {
-
+	/**
+	 * attribut qui represente le joueur
+	 */
+	private Joueur joueur;
+	
+	/**
+	 * attribut qui represente le labyrinthe
+	 */
+	private Labyrinthe laby;
+	
+	/**
+	 * constructeur par defaut
+	 */
+	public Jeu() {
+		this.laby = new Labyrinthe();
+		this.joueur = new Joueur(this.laby.getEntreeX(), this.laby.getEntreeY());
+	}
+	
+	/**
+	 * classe permettant de deplacer le joueur dans le labyrinthe
+	 * 
+	 * @param direction la direction prise par le joueur (direction cardinale representee par un entier)
+	 */
+	public void deplacerJoueur(int direction) {
+		int xNewPos = this.joueur.getPosX();
+		int yNewPos = this.joueur.getPosY();
+		
+		/**
+		 * 0 : Up
+		 * 1 : Down
+		 * 2 : Left
+		 * 3 : Right
+		 */
+		switch(direction) {
+			case 0:
+				yNewPos--;
+				break;
+				
+			case 1:
+				yNewPos++;
+				break;
+				
+			case 2:
+				xNewPos--;
+				break;
+				
+			case 3:
+				xNewPos++;
+				break;
+				
+			default:
+				break;
+		} 
+		
+		//on recupere le type de la case sur laquelle on veut avancer
+		int type = this.laby.getCase(xNewPos, yNewPos).getType();
+		
+		//le joueur ne poeut avancer que si la case est vide
+		if(type == Case.TYPE_VIDE) {
+			this.joueur.seDeplacer(xNewPos, yNewPos);
+		}
+	}
+	
+	/**
+	 * getter de l'attribut joueur
+	 * 
+	 * @return l'attribut joueur
+	 */
+	public Joueur getJoueur() { return this.joueur; }
+	
+	/**
+	 * getter de l'attribut laby
+	 * 
+	 * @return l'attribut laby
+	 */
+	public Labyrinthe getLaby() { return this.laby; }
 }
---
+++ projet_zeldiablo/src/Test/TestJeu.java
@@ -0,0 +1,41 @@
+package Test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import Elements.Jeu;
+
+public class TestJeu {
+	
+	/**
+	 * test quand le joueur se deplace dans le vide
+	 */
+	@Test
+	public void methode_seDeplacer_vide() {
+		//initialisation des valeurs
+		Jeu j = new Jeu();
+		
+		//test de la methode
+		int yOld = j.getJoueur().getPosY();
+		j.deplacerJoueur(1);
+		
+		assertEquals("Le joueur doit s'etre deplace vers le bas", yOld + 1, j.getJoueur().getPosY());
+	}
+	
+	/**
+	 * test quand le joueur se deplace dans un mur
+	 */
+	@Test
+	public void methode_seDeplacer_mur() {
+		//initialisation des valeurs
+		Jeu j = new Jeu();
+		
+		//test de la methode, on avance le personnage assez pour etre sur d'avoir rencontre un mur
+		for(int i = 0; i < 20; i++) {
+			j.deplacerJoueur(2);
+		}
+		
+		assertEquals("Le joueur doit etre au bord du labyrinthe, donc a x = 1", 1, j.getJoueur().getPosX());
+	}
+}
