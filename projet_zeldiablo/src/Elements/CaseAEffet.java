013d266c86fe9eed79bada70d8e72f3d023f1a3b
Léo Muller <leomllr8@gmail.com>
2020-09-01 17:51:16 +0200

    ajout de la javadoc

--- projet_zeldiablo/src/Elements/CaseAEffet.java
+++ projet_zeldiablo/src/Elements/CaseAEffet.java
@@ -1,10 +1,26 @@
 package Elements;
 
+/**
+ * classe representant une case avec un effet
+ *
+ */
 public abstract class CaseAEffet extends Case {
-
+	
+	/**
+	 * constructeur d'une case a effet
+	 * 
+	 * @param x abscisse de la case
+	 * @param y ordonnee de la case
+	 * @param type type de la case
+	 */
 	public CaseAEffet(int x, int y, int type) {
 		super(x, y, type);
 	}
 	
+	/**
+	 * methode abstraite permettant de declencher l'effet de la case
+	 * 
+	 * @param e l'entite ayant marche sur la case
+	 */
 	public abstract void activerEffet(Entite e);
 }
--- projet_zeldiablo/src/Elements/CasePiege.java
+++ projet_zeldiablo/src/Elements/CasePiege.java
@@ -1,13 +1,32 @@
 package Elements;
 
+/**
+ * classe representant une case piege
+ *
+ */
 public class CasePiege extends CaseAEffet {
+	/**
+	 * attribut permettant de savoir si le piege a deja ete declenche une fois
+	 */
 	private boolean active;
-
+	
+	/**
+	 * constructeur d'une case piege
+	 * 
+	 * @param x abscisse de la case
+	 * @param y ordonne de la case
+	 * @param type type de la case
+	 */
 	public CasePiege(int x, int y, int type) {
 		super(x, y, type);
 		this.active = false;
 	}
 
+	/**
+	 * methode permettant de blesser l'entite ayant marche sur la case
+	 * 
+	 * @param e entite ayant marche sur la case
+	 */
 	@Override
 	public void activerEffet(Entite e) {
 		e.vie -= 1;
