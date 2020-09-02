d12d1bfc24b61b6ce3e117880e3badc43e60ef89
hugo_etchevarne <hugo.etchevarne@gmail.com>
2020-09-02 09:34:41 +0200

    ecriture de la classe niveau

--- projet_zeldiablo/src/Elements/Niveau.java
+++ projet_zeldiablo/src/Elements/Niveau.java
@@ -1,37 +1,41 @@
 package Elements;
 
-import java.util.ArrayList;
+import java.io.FileOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.List;
 
-public class Niveau {
 
-	public List<Labyrinthe> labyrinthes;
 
+public class Niveau implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	public Labyrinthe labyrinthes;
+	public List<Entite> monstres;
 	private boolean fin;
 
-	public Niveau(List<Labyrinthe> labyrinthes) {
+	public Niveau(Labyrinthe labyrinthes) {
 		this.labyrinthes = labyrinthes;
 
 	}
-
-	public Niveau() {
-		this.labyrinthes = new ArrayList<>();
-	}
-
-	public void ajouterLaby(Labyrinthe l) {
-		labyrinthes.add(l);
+	
+	public void sauvegarder(String nomSave) throws FileNotFoundException, Exception {
+		String ext ="";
+		if(nomSave.length()>4) {
+			String nom_ext =nomSave.substring(nomSave.length()-4, nomSave.length());
+			System.out.println(nom_ext);
+			if(!nom_ext.equals(".niv")) {
+				ext=".niv";
+			}
+		}
+		
+		ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(new File(nomSave)));
+		stream.writeObject(this);
+		
 	}
 	
-	
-	 public Labyrinthe getCurentLaby() {
-	        for (Labyrinthe l : labyrinthes) {
-	            if (l.getCase(1, 1) != null) {
-	                return l;
-	            }
-	        }
-	        return null;
-	    }
-
 	public boolean isFinish() {
 		return fin;
 	}
