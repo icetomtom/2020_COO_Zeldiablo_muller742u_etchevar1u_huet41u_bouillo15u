ac3cc81de03ce13f743332b19a42f0644070cb49
hugo_etchevarne <hugo.etchevarne@gmail.com>
2020-09-03 10:45:54 +0200

    modification de la classe bouclier

--- projet_zeldiablo/src/Elements/Boucliers.java
+++ projet_zeldiablo/src/Elements/Boucliers.java
@@ -1,5 +1,13 @@
 package Elements;
 
-public class Boucliers {
+public class Boucliers extends Arme {
+	
+	public Boucliers() {
+		this.resistance = 15;
+	}
+	
+	public String getNomObjet() {
+		return "Bouclier";
+	}
 
 }
