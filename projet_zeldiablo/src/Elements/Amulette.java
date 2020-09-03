b8cdfe05652038ed6ce2ec39ba26e200d0666ae8
Léo Muller <leomllr8@gmail.com>
2020-09-03 14:38:24 +0200

    correction de l'interface Objet

---
+++ projet_zeldiablo/src/Elements/Amulette.java
@@ -0,0 +1,16 @@
+package Elements;
+
+public class Amulette implements Objet {
+
+	@Override
+	public String getNomObjet() {
+		return "Amulette";
+	}
+
+	@Override
+	public boolean etreUtilise(Joueur j) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
