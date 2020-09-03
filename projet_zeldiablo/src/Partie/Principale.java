01fea8e6c072543069f231ba0c4275734c8877aa
Thomas Bouillon <tbouillon54@gmail.com>
2020-09-03 15:31:02 +0200

    Correction de generation Labyrinthe

--- projet_zeldiablo/src/Partie/Principale.java
+++ projet_zeldiablo/src/Partie/Principale.java
@@ -15,18 +15,19 @@ public class Principale {
         Jeu j = new Jeu();
 
         Sprites.chargerFeuille("entities", "images/16x16DungeonTileset.v3.png", 16, 16);
-        
+
         CasePiege cp = new CasePiege(1, 1);
         Porte p = new Porte(5, 6);
         CaseSecrete o = new CaseSecrete(8, 8, p, Case.TYPE_OUVERTURE);
         CaseSecrete f = new CaseSecrete(8, 7, p, Case.TYPE_FERMETURE);
-        
+
         j.getLaby().setCase(cp);
         j.getLaby().setCase(p);
         j.getLaby().setCase(o);
         j.getLaby().setCase(f);
         
         DessinPartie dessin = new DessinPartie();
+
         for(Case c : j.getLaby().getCases()) {
             dessin.addSprite(c);
         }
@@ -34,6 +35,7 @@ public class Principale {
 
         MoteurGraphique m = new MoteurGraphique(j, dessin);
 
+        // Menu
         m.lancerJeu(600, 600, 8);
     }
 }
