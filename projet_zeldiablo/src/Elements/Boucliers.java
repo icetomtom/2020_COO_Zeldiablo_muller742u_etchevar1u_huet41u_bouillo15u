fd9636a87f06314775e9e4b7456e7645d2166fd0
hugo_etchevarne <hugo.etchevarne@gmail.com>
2020-09-04 08:13:15 +0200

    ajout de la javadoc

--- projet_zeldiablo/src/Elements/Boucliers.java
+++ projet_zeldiablo/src/Elements/Boucliers.java
@@ -2,10 +2,16 @@ package Elements;
 
 public class Boucliers extends Arme {
 	
+	/**
+	 * getter retournant le nom de l objet
+	 */
 	public String getNomObjet() {
 		return "Bouclier";
 	}
 
+	/**
+	 * methode retournant si l objet peut etre utiliser
+	 */
 	@Override
 	public boolean etreUtilise(Joueur j) {
 		// TODO Auto-generated method stub
--- projet_zeldiablo/src/Elements/Objet.java
+++ projet_zeldiablo/src/Elements/Objet.java
@@ -1,7 +1,15 @@
 package Elements;
 
 public interface Objet {
+	
+	/**
+	 * methode abstraite donnant le nom de l objet
+	 */
 	public abstract String getNomObjet();
-
+	
+	/**
+	 * methode abstraite donnant si l objet peut etre utiliser 
+	 * @param j
+	 */
 	public abstract boolean etreUtilise(Joueur j);
 }
