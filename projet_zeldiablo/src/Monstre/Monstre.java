edbfc3faa8dbd5b0ddfe8b76484df5ab9b14525b
hugo_etchevarne <hugo.etchevarne@gmail.com>
2020-09-04 14:33:49 +0200

    javadoc

--- projet_zeldiablo/src/Monstre/Monstre.java
+++ projet_zeldiablo/src/Monstre/Monstre.java
@@ -2,8 +2,16 @@ package Monstre;
 
 import Elements.Entite;
 
+/**
+ * classe representant un monstre
+ *
+ */
 public class Monstre extends Entite {
 
+	/**
+	 * constructeur
+	 * @param nom
+	 */
 	public Monstre(String nom) {
 		super(nom);
 	}
